fe9e0ec828336ff63cdb1152d8952b088ce61e04
Rob <Rob@LAPTOP-D04HPS34>
2019-03-22 14:29:49 +0000

    product_edit changed to reflect currencies

--- view/product_edit.php
+++ view/product_edit.php
@@ -1,29 +1,29 @@
 <?php include './include/header.php'; ?>
 <main>
-    <h2>Edit Product</h2>
-    <form action="index.php" method="post" id="add_product_form">
+    <h2>Edit Currency</h2>
+    <form action="index.php" method="post" id="add_currency_form">
 
-        <input type="hidden" name="action" value="update_product">
+        <input type="hidden" name="action" value="update_currency">
 
-        <input type="hidden" name="productCode" value="<?php echo $product['product_code'] ?>">
+        <input type="hidden" name="currencyCode" value="<?php echo $currency['currency_code'] ?>">
 
         <label>Product Name:</label>
-        <input type="input" name="productName" value="<?php echo $product['product_name']; ?>">
+        <input type="input" name="currencyName" value="<?php echo $currency['currency_name']; ?>">
         <br>
 
         <label>Description:</label>
-        <input type="input" name="productDescription" value="<?php echo $product['product_description']; ?>">
+        <input type="input" name="currencyDescription" value="<?php echo $currency['currency_description']; ?>">
         <br>
 
         <label>Price:</label>
-        <input type="input" name="productPrice" value="<?php echo $product['product_price']; ?>">
+        <input type="input" name="currencyPrice" value="<?php echo $currency['currency_price']; ?>">
         <br>
 
         <hr/>
         <input type="submit" value="Save Changes">
         <br>
     </form>
-    <p><a href="index.php?action=list_products">View Product List</a></p>
+    <p><a href="index.php?action=list_currencies">View Currency List</a></p>
 
 </main>
 <?php include './include/footer.php'; ?>
