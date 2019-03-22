f6b5423cee7abf2c1d21c6c8169d757e6b5844c2
Rob <Rob@LAPTOP-D04HPS34>
2019-03-22 14:34:34 +0000

    product_list changed to reflect currency

--- view/product_list.php
+++ view/product_list.php
@@ -1,42 +1,42 @@
 <?php include './include/header.php'; ?>
 <main>
 
-    <h2>Financial Products List</h2>
+    <h2>Financial Currencies List</h2>
     <section>
         <table>
             <tr>
-                <th>Product Name</th>
+                <th>Currency Name</th>
                 <th>Description</th>
                 <th>Price</th>
                 <th>&nbsp;</th>
                 <th>&nbsp;</th>
             </tr>
-            <?php foreach ($products as $product) : ?>
+            <?php foreach ($currencies as $currency) : ?>
                 <tr>
-                    <td><?php echo $product['product_name']; ?></td>    
-                    <td><?php echo $product['product_description']; ?></td>
-                    <td><?php echo $product['product_price']; ?></td>   
+                    <td><?php echo $currency['currency_name']; ?></td>    
+                    <td><?php echo $currency['currency_description']; ?></td>
+                    <td><?php echo $currency['currency_price']; ?></td>   
                     <td>
                         <form action="." method="post">
-                            <input type="hidden" name="action" value="edit_product">
-                            <input type="hidden" name="productCode" value="<?php echo $product['product_code']; ?>">
+                            <input type="hidden" name="action" value="edit_currency">
+                            <input type="hidden" name="currencyCode" value="<?php echo $currency['currency_code']; ?>">
                             <input type="submit" value="Edit">
                         </form>
                     </td>
                     <td>
                         <form action="." method="post">
-                            <input type="hidden" name="action" value="delete_product">
-                            <input type="hidden" name="productCode" value="<?php echo $product['product_code']; ?>">
+                            <input type="hidden" name="action" value="delete_currency">
+                            <input type="hidden" name="currencyCode" value="<?php echo $currency['currency_code']; ?>">
                             <input type="submit" value="Delete">
                         </form>
                     </td>
                 </tr>
             <?php endforeach; ?>
         </table>
-        <p><a href="?action=list_products">List Products</a></p>
-        <p><a href="?action=add_product_form">Add Product</a></p>
+        <p><a href="?action=list_currencies">List Currencies</a></p>
+        <p><a href="?action=add_currency_form">Add Currency</a></p>
         <p><a href="view/userLogIn.php">Quick Check Balance</a></p>
-        <p><a href="?action=buy_product_form">Buy Product</a></p>
+        <p><a href="?action=buy_currency_form">Buy Currency</a></p>
         <p><a href="?action=register_new_form">Register</a></p>
         <p><a href="?action=user_cancellation_form">Account Cancellation</a></p>
     </section>
