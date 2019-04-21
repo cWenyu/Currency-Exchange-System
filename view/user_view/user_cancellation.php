6a47e6709f8483bdbc04b9b7add1174e1967131a
cWenyu <cWenyu@users.noreply.github.com>
2019-04-22 00:47:29 +0100

    finish account cancellation bootstrap

--- view/user_view/user_cancellation.php
+++ view/user_view/user_cancellation.php
@@ -1,26 +1,93 @@
-<?php include './include/header.php'; ?>
-<main>
-    <h2>Cancellation</h2>
-    <h3>
-        Please enter your information to make sure your operation.
-    </h3>
-    <form action="index.php" method="post" id="user_cancellation_form">
-        <input type="hidden" name="action" value="account_cancellation">
+<?php
+$title = "Cancellation";
+include './include/header.php';
+session_start();
+?>
+<body>
+    <div id="wrapper" class="home-page">
+        <!-- start header -->
+        <header>
+            <div class="navbar navbar-default navbar-static-top">
+                <div class="container">
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <a class="navbar-brand" href="index.php"><img src="img/logo_1.png" alt="logo"/></a>
+                    </div>
+                    <div class="navbar-collapse collapse ">
+                        <ul class="nav navbar-nav">
+                            <li><a href="index.php">Home</a></li> 
+                            <li><a href="?action=user_login">Login</a></li>
+                            <li><a href="?action=register_new_form">Register</a></li>
+                            <li class="active"><a href="?action=user_cancellation_form">Account Cancellation</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <!-- end header -->
+        <section id="inner-headline">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h2 class="pageTitle">Account Cancellation</h2>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+    <div class="container">
+        <h2>Cancellation</h2>
+        <div class="col-md-6 log">			 
 
-        <!-- next step: regex for each input-->
-        <label>Register Number:</label>
-        <input type="input" name="registerNumber" placeholder="Register Number" pattern="^[0-9]*$" required autofocus>
-        <br>
+            <form action="index.php" method="post" id="user_cancellation_form">
+                <input type="hidden" name="action" value="account_cancellation">
+                <div class="col-md-6">
+                    <h5>Register Number:</h5>
 
-        <label>Register Password:</label>
-        <input type="password" name="registerPassword" pattern="^([a-zA-Z0-9!%^&*_@#~]){8,16}$" required autofocus>
-        <br>
+                    <div>
+                        <input placeholder="Register Number" name="registerNumber" id="registerNumber" pattern="[0-9]*$"/>
+                    </div>
 
-        <hr/>
-        <label>&nbsp;</label>
-        <input type="submit" value="Confirm">
-        <br>
-    </form>
-    <p><a href="index.php?action=list_currencies">List Currencies</a></p> 
-</main>
+                    <h5>Register Password:</h5>
+                    <div>
+                        <input type="password" placeholder="Register Password" name="registerPassword" id="registerPassword" pattern="^([a-zA-Z0-9!%^&*_@#~]){8,16}$" required autofocus />
+                    </div>  
+                    <br>
+                    <button type="button" class="warning_buttons" data-toggle="modal" data-target="#exampleModal">
+                        Confirm
+                    </button>
+
+                    <!-- dialog box -->
+                    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="exampleModalLabel">Account </h5>
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                <div class="modal-body">
+                                    Please click confirm to make sure your operation.
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                    <button type="submit" id="warning_button">Confirm</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
+                </div>
+            </form> 				 
+        </div>		 
+    </div>
+
+
+</body>
 <?php include './include/footer.php'; ?>
