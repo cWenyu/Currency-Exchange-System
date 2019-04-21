5b1e42a743954fc3d462d678b96eb9b306033fd5
cWenyu <cWenyu@users.noreply.github.com>
2019-04-22 00:47:07 +0100

    finish index(currency list) bootstrap

--- view/index_view/currency_list.php
+++ view/index_view/currency_list.php
@@ -1,30 +1,173 @@
-<?php include './include/header.php'; ?>
-<main>
+<?php
+$title = 'Home';
+include 'include/header.php';
+?>
+<body>
+    <div id="wrapper" class="home-page">
+        <!-- start header -->
+        <header>
+            <div class="navbar navbar-default navbar-static-top">
+                <div class="container">
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <a class="navbar-brand" href="index.php"><img src="img/logo_1.png" alt="logo"/></a>
+                    </div>
+                    <div class="navbar-collapse collapse ">
+                        <ul class="nav navbar-nav">
+                            <li class="active"><a href="index.php">Home</a></li> 
+                            <li><a href="?action=user_login">Login</a></li>
+                            <li><a href="?action=register_new_form">Register</a></li>
+                            <li><a href="?action=user_cancellation_form">Account Cancellation</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <!-- end header -->
 
-    <h2>Financial Currencies List</h2>
-    <section>
-        <table>
-            <tr>
-                <th>Currency Name</th>
-                <th>Description</th>
-                <th>Price</th>
-                <th>&nbsp;</th>
-                <th>&nbsp;</th>
-            </tr>
-            <?php foreach ($currencies as $currency) : ?>
-                <tr>
-                    <td><?php echo $currency['currency_name']; ?></td>    
-                    <td><?php echo $currency['currency_description']; ?></td>
-                    <td><?php echo $currency['currency_price']; ?></td>   
+        <section id="banner">
 
-                </tr>
-            <?php endforeach; ?>
-        </table>
-        <p><a href="view/user_view/userLogIn.php">Login</a></p>
-        <p><a href="?action=register_new_form">Register</a></p>
-        <p><a href="?action=user_cancellation_form">Account Cancellation</a></p>
-    </section>
-</section>
+            <!-- Slider -->
+            <div id="main-slider" class="flexslider">
+                <ul class="slides">
+                    <li>
+                        <img src="img/slides/1.jpg" alt="flexslider" /> 
+                        <div class="flex-caption">
+                            <h3>CONSULTING</h3> 
+                            <p>Real-time currency rate</p>  
+                        </div> 
+                    </li>
+                    <li>
+                        <img src="img/slides/2.jpg" alt="flexslider" /> 
+                        <div class="flex-caption">
+                            <h3>FINANCIAL</h3> 
+                            <p>Online currency exchange</p>  
+                        </div> 
+                    </li>
+                </ul>
+            </div>
+            <!-- end slider -->
 
-</main>
-<?php include './include/footer.php'; ?>
+        </section>  
+
+        <section class="jumbobox">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div><h1>Financial Currencies List</h1></div>
+                        <div>
+                            <table class="table table-striped">
+                                <thead class="thead-dark">
+                                    <tr>
+                                        <th>Currency Name</th>
+                                        <th>Description</th>
+                                        <th>Price</th>
+                                    </tr>  
+                                </thead>
+
+                                <tbody class="tbody">
+                                    <?php foreach ($currencies as $currency) : ?>
+                                        <tr>
+                                            <th><?php echo $currency['currency_name']; ?></th>    
+                                            <td><?php echo $currency['currency_description']; ?></td>
+                                            <td><?php echo $currency['currency_price']; ?></td>   
+
+                                        </tr>
+                                    <?php endforeach; ?> 
+
+                                </tbody>
+
+
+                            </table> 
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section> 
+
+
+        <!-- cooperators logo -->
+        <section class="clients">
+            <div class="container">
+                <div class="clients-slider">
+                    <h3 class="header-title">Our Cooperators</h3>
+                    <div class="clients-control pull-right">
+                        <a class="prev btn btn-gray btn-xs"><i class="fa fa-angle-left fa-2x"></i></a>
+                        <a class="next btn btn-gray btn-xs"><i class="fa fa-angle-right fa-2x"></i></a>
+                    </div>
+                    <span class="line"></span>
+                    <div id="clients-slider" class="owl-carousel">
+                        <div class="item">
+                            <a href="https://aib.ie/" target="_blank">
+                                <img alt="Our Client" src="img/OurCooperators/aib-grey.jpg">
+                                <img alt="Our Client" src="img/OurCooperators/aib.jpg" class="colored">
+                            </a>
+                        </div>
+
+
+                        <div class="item">
+                            <a href="https://digital.ulsterbank.ie/" target="_blank">
+                                <img alt="Our Client" src="img/OurCooperators/ub-grey.png">
+                                <img alt="Our Client" src="img/OurCooperators/ub.png" class="colored">
+                            </a>
+                        </div>
+
+                        <div class="item">
+                            <a href="https://www.bankofireland.com/" target="_blank">
+                                <img alt="Our Client" src="img/OurCooperators/boi-grey.png">
+                                <img alt="Our Client" src="img/OurCooperators/boi.png" class="colored">
+                            </a>
+                        </div>
+
+                        <div class="item">
+                            <a href="https://www.wyelandsbank.co.uk/" target="_blank">
+                                <img alt="Our Client" src="img/OurCooperators/wbc-grey.png">
+                                <img alt="Our Client" src="img/OurCooperators/wbc.png" class="colored">
+                            </a>
+                        </div>
+
+                        <div class="item">
+                            <a href="" target="_blank">
+                                <img alt="Our Client" src="img/OurCooperators/visa-grey.png">
+                                <img alt="Our Client" src="img/OurCooperators/visa.png" class="colored">
+                            </a>
+                        </div>
+
+                        <div class="item">
+                            <a href="" target="_blank">
+                                <img alt="Our Client" src="img/OurCooperators/up-grey.png">
+                                <img alt="Our Client" src="img/OurCooperators/up.png" class="colored">
+                            </a>
+                        </div>
+
+                        <div class="item">
+                            <a href="https://www.british-business-bank.co.uk/" target="_blank">
+                                <img alt="Our Client" src="img/OurCooperators/bbb-grey.png">
+                                <img alt="Our Client" src="img/OurCooperators/bbb.png" class="colored">
+                            </a>
+                        </div>
+                        <div class="item">
+                            <a href="http://www.boc.cn/en/index.html" target="_blank">
+                                <img alt="Our Client" src="img/OurCooperators/boc-grey.png">
+                                <img alt="Our Client" src="img/OurCooperators/boc.png" class="colored">
+                            </a>
+                        </div>
+
+                        <div class="item">
+                            <a href="https://www.bankofengland.co.uk/" target="_blank">
+                                <img alt="Our Client" src="img/OurCooperators/boe-grey.png">
+                                <img alt="Our Client" src="img/OurCooperators/boe.png" class="colored">
+                            </a>
+                        </div>
+                        <div class="fullwidth margin-bottom-20">
+                        </div>
+                    </div>
+                </div>
+        </section>
+    </div>
+    <?php include 'include/footer.php'; ?>
+</body>    
